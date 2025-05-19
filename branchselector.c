531ed72589fbe8fe411c155b4629b00143916cb7
Sizils5932 <dev.bharti@s.amity.edu>
2025-05-19 12:39:36 +0530

    added another case in program

--- branchselector.c
+++ branchselector.c
@@ -9,10 +9,10 @@ int main()
         printf("4. B.Tech ECE\n");
         printf("5. B.Tech Mechanical\n");
 	printf("6. B.Tech CSE(AI&ML)\n");
-        printf("7. Exit\n");
+	printf("7. B.Tech ECE");
+        printf("8. Exit\n");
 	printf("Select your desired branch of engineering");
 	scanf("%d",choose);
-
 	switch (choose) {
         case 1:
             printf("You selected B.Tech CSE\n");
@@ -31,12 +31,13 @@ int main()
             return 0;
 	case 6:
 	    printf("You have selected B.Tech CSE(AI&ML)\n");
-	case 7:
+        case 7:
+	    printf("You have selected B.Tech ECE\n");
+	case 8:
 	    printf("Exit");
         default:
             printf("Invalid choice. Try again.\n");
     }
-
     return 0;
 }
 
